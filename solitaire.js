404e0be211c9beb71af1f5570cdab8501d96fa2e
Matthew LeMay <kazaaknet@yahoo.com>
2022-06-06 14:09:14 -0600

    .

--- solitaire.js
+++ solitaire.js
@@ -90,18 +90,6 @@ function playGame() {
     //  the card, e.g., AC, 2C, etc.
     let upSuits = [[],[],[],[]];
 
-    upSuits = [ [ 'AH' ], [ 'AD' ], [ 'AC' ], [ 'AS' ] ];
-    board = [
-        [ [ 'KC', true ], [ 'QD', true ], [ 'JC', true ], [ '10D', true ], [ '9C', true ], [ '8D', true ], [ '7C', true ], [ '6D', true ], [ '5C', true ], [ '4D', true ], [ '3C', true ], [ '2D', true ] ],
-        [ [ 'KD', true ], [ 'QC', true ], [ 'JD', true ], [ '10C', true ], [ '9D', true ], [ '8C', true ], [ '7D', true ], [ '6C', true ], [ '5D', true ], [ '4C', true ], [ '3D', true ], [ '2C', true ] ],
-        [ [ 'KS', true ], [ 'QH', true ], [ 'JS', true ], [ '10H', true ], [ '9S', true ], [ '8H', true ], [ '7S', true ], [ '6H', true ], [ '5S', true ], [ '4H', true ], [ '3S', true ], [ '2H', true ] ],
-        [ [ 'KH', true ], [ 'QS', true ], [ 'JH', true ], [ '10S', true ], [ '9H', true ], [ '8S', true ], [ '7H', true ], [ '6S', true ], [ '5H', true ], [ '4S', true ], [ '3H', true ], [ '2S', true ] ],
-        [ ],
-        [ ],
-        [ ]
-    ];
-    deck = [];
-
     // game loop!
     let lastLineCount = 0;
     let extraLineCount = 0;
